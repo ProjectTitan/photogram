3ff81948a1c10d7e989ab4a0627ebd6e0e1d1938
test@example.com <test@example.com>
2015-10-09 12:20:38 -0500

    crud work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,21 @@
 class PhotosController < ApplicationController
   def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    photo = Photo.new
+    photo.source = params[:the_source]
+    photo.caption = params[:the_caption]
+    photo.save
+    redirect_to "/photos/new"
+  end
+
+  def index
+    @photos = Photo.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # Routes to READ photos
+
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+  get("/index",       { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 end
